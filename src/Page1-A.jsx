fa18d630893985ec8dbb91a5c957fd8577fcb4be
hiro <hmaeyama0510@gmail.com>
2021-05-28 04:51:39 +0900

    add useHistory hooks

--- src/Page1-A.jsx
+++ src/Page1-A.jsx
@@ -1,12 +1,18 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1A = () => {
   const { state } = useLocation();
   console.log(state);
 
+  const history = useHistory();
+
+  const handleBack = () => history.goBack();
+
   return (
     <div>
       <h1>Page1-A</h1>
+      <br />
+      <button onClick={handleBack}>Back</button>
     </div>
   );
 };
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const history = useHistory();
+
+  const handleClick = () => history.push("/page1/a");
 
   return (
     <div>
@@ -10,6 +14,8 @@ export const Page1 = () => {
       <Link to={{ pathname: "/page1/a", state: arr }}>Page1-A</Link>
       <br />
       <Link to="/page1/b">Page1-B</Link>
+      <br />
+      <button onClick={handleClick}>Page1-A</button>
     </div>
   );
 };
