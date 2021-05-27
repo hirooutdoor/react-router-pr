e4288d1e536a6a566a362c3d180440cef3b9d8f7
hiro <hmaeyama0510@gmail.com>
2021-05-27 19:13:47 +0900

    import  Browser Router to enable the function of routing where you embrace tha tags by BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import "./styles.css";
-import { Home } from "./Home";
+import { BrowserRouter } from "react-router-dom";
+
 import { Page1 } from "./Page1";
+import { Home } from "./Home";
 import { Page2 } from "./Page2";
+import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Home />
+        <Page1 />
+        <Page2 />
+      </div>
+    </BrowserRouter>
   );
 }
